b43ff5202c9c505471b112d11bf66838ecd19aeb
Руслан <ruslan.nasibullin.00@gmail.com>
2020-07-03 16:33:59 +0700

    Added auth domain step

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,3 +1,4 @@
+import com.google.inject.internal.cglib.core.$AbstractClassGenerator;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -6,7 +7,13 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Bot extends TelegramLongPollingBot {
+    HashMap <Long, Boolean> auth = new HashMap<>();
+
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
@@ -22,33 +29,62 @@ public class Bot extends TelegramLongPollingBot {
         Message message = update.getMessage();
 
         SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
+        Long curChatId = message.getChatId();
+        sendMessage.setChatId(curChatId.toString());
 
         if (message != null && message.hasText()) {
-            switch (message.getText()) {
-                case "/start":
-                    try {
-                        execute(sendMessage.setText("Oh, hello there \nWrite /help for more info :0"));
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
+            String msgText = message.getText();
+
+            if (msgText.equals("/start")) {
+                try {
+                    auth.put(curChatId, false);
+                    execute(sendMessage.setText("Oh, hello there \nWrite /help for more info :0"));
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            else if (msgText.equals("/help")) {
+                try {
+                    execute(sendMessage.setText("Here you can configure your notifications at Jira. Write /auth to start your authorization."));
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            else if (msgText.equals("/auth")) {
+                try {
+                    execute(sendMessage.setText("Firstly, we need to know your personal Jira domain. " +
+                            "Usually it looks like <SOME_NAME>.atlassian.net " +
+                            "so just write it to me in the same format."));
+                    auth.put(curChatId, true);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Means that user have chosen /auth on prev step so he should write his domain
+            else if (auth.get(curChatId)) {
+                try {
+                    Pattern pattern = Pattern.compile("^.+[.]atlassian[.]net$");
+                    Matcher matcher = pattern.matcher(message.getText());
+                    if (matcher.matches()) {
+                        execute(sendMessage.setText("Good!"));
+                        auth.put(curChatId, false);
                     }
-                    break;
-                case "/help":
-                    try {
-                        execute(sendMessage.setText("Here you can configure your notifications at Jira"));
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                default:
-                    try {
-                        execute(sendMessage.setText("I'm actually don't understand, dude :-/"));
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            else {
+                try {
+                    execute(sendMessage.setText("I'm actually don't understand, dude :-/)"));
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
             }
         }
-
     }
 
     public String getBotUsername() {
